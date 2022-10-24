9ef830970489be9f68b5f598d62fe599a4ec11c8
Meggo <105523419+ItsMeggo@users.noreply.github.com>
2022-10-24 11:20:40 +0100

    add high score attribute

--- scoreboard.py
+++ scoreboard.py
@@ -6,6 +6,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.color("white")
